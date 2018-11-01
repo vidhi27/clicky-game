a3c0064fc760148770bbbb16126889e55656564d
vidhi <panchalvidhi@ymail.com>
2018-10-31 21:14:10 -0400

    Updates

---
+++ precache-manifest.75f24a9144e61d2d52b8f9f2279cd875.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "f6474b558cd7aca93671",
+    "url": "/clicky-game/static/js/main.f6474b55.chunk.js"
+  },
+  {
+    "revision": "1a687757c0b8ee94960a",
+    "url": "/clicky-game/static/js/1.1a687757.chunk.js"
+  },
+  {
+    "revision": "f6474b558cd7aca93671",
+    "url": "/clicky-game/static/css/main.d4a88412.chunk.css"
+  },
+  {
+    "revision": "1a687757c0b8ee94960a",
+    "url": "/clicky-game/static/css/1.ec6a48a2.chunk.css"
+  },
+  {
+    "revision": "ac0a18c6c7ebfc3a96e07d32291b90e7",
+    "url": "/clicky-game/index.html"
+  }
+];
